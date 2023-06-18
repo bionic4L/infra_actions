7941ccc957ab35c4e427c2060fa3b03437f10bf5
umarak <screaminglhf@gmail.com>
2023-06-18 19:43:06 +0300

    test commit 11

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -1,5 +1,5 @@
+from http import HTTPStatus
 from django.test import Client, TestCase
-
 
 class StaticPagesURLTests(TestCase):
     def setUp(self):
@@ -8,11 +8,15 @@ class StaticPagesURLTests(TestCase):
     def test_about_url_exists_at_desired_location(self):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
-        self.assertEqual(
-            response.status_code, 404
-        )
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
-        response = self.guest_client.get('/second_page/')
-        self.assertEqual(
-            response.status_code, 404
-        )
+        response = self.guest_client.get('/second/')
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_page_shows_correct_content(self):
+        """Проверка контента страниц."""
+        response = self.guest_client.get('/')
+        self.assertContains(response, 'У меня получилось!')
+
+        response = self.guest_client.get('/second/')
+        self.assertContains(response, 'А это вторая страница')
